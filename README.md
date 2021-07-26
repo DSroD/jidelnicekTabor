e1f4b6eded370d1cb609a2743bba90ff87dfd7b8
strelda <strelecekja@gmail.com>
2021-07-26 11:19:23 +0200

    rezepsání jídel

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# jidelnicekTabor
+# jidelnicekTabor 2021
+Recepty jsou rozepsány v jsonu, skript v julii pak přepočítává množství s volným parametrem žravosti.
---
+++ jidelnicek.jl
@@ -0,0 +1,8 @@
+# 34+10 porcí
+# 2 porce bez masa vedle
+import JSON
+
+
+jid = JSON.parsefile("jidelnicek.json")
+# print both dictionaries
+jid["2"]["vecere"]["co"]
